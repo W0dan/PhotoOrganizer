99668162b8f3c2649cc382af64521b354ff62804
W0dan <W0dan.git@gmail.com>
2019-08-19 20:18:50 +0200

    initial

---
+++ PhotoOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var path = @"W:\Fotos\2019\08\Part004_WeekOldeKampNederland";
+
+            var unfilteredPath = System.IO.Path.Combine(path, "Unfiltered");
+            var RAWPath = System.IO.Path.Combine(path, "RAW");
+            var PhotoshopPath = System.IO.Path.Combine(path, "Photoshop");
+
+            var di = new System.IO.DirectoryInfo(unfilteredPath);
+
+            var counter = 0;
+
+            var imageFiles = di.GetFiles("*.JPG").OrderBy(x => x.Name).ToList();
+
+            var taskList = new List<Task>();
+            foreach (var fi in imageFiles)
+            {
+                if (counter > 100) break;
+
+                Debug.WriteLine(fi.Name);
+
+                taskList.Add(AddThumbnail(fi));
+
+                counter++;
+            }
+
+            foreach(var t in taskList)
+                await t;
+        }
+
+        private async Task AddThumbnail(System.IO.FileInfo fi)
+        {
+            var thumbnail = await PhotoThumbnail.CreateThumbnail(ThumbNailsStackPanel, fi.FullName);
+
+            //ThumbNailsStackPanel.Children.Add(thumbnail);
+        }
+    }
+}
---
+++ PhotoOrganizer/PhotoThumbnail.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace PhotoOrganizer
+{
+    /// <summary>
+    /// Interaction logic for PhotoThumbnail.xaml
+    /// </summary>
+    public partial class PhotoThumbnail : UserControl
+    {
+        public PhotoThumbnail()
+        {
+            InitializeComponent();
+        }
+
+        public static async Task<PhotoThumbnail> CreateThumbnail(WrapPanel panel, string uri)
+        {
+            var result = new PhotoThumbnail();
+            if (panel.Dispatcher.Thread == Thread.CurrentThread)
+            {
+                panel.Children.Add(result);
+            }
+            else
+            {
+                await panel.Dispatcher.BeginInvoke(new Func<UIElement, int>(panel.Children.Add), result);
+            }
+
+            if (uri != null && File.Exists(uri))
+            {
+                await Task.Run(() => LoadThumbnail(result, uri));
+            }
+
+            return result;
+        }
+
+        private static async Task LoadThumbnail(PhotoThumbnail thumbnailControl, string uri)
+        {
+            if (thumbnailControl.Dispatcher.Thread == Thread.CurrentThread)
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = new Uri(uri);
+                image.DecodePixelWidth = 90;
+                image.EndInit();
+
+                thumbnailControl.ThumbnailImage.Source = image;
+            }
+            else
+            {
+                await thumbnailControl.Dispatcher.BeginInvoke(new Func<PhotoThumbnail, string, Task>(LoadThumbnail), thumbnailControl, uri);
+            }
+        }
+    }
+}
